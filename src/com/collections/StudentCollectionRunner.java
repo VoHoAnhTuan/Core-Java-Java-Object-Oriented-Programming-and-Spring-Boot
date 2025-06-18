df88427e964b03071046111990cecb6c50ce1f3b
Tuan Vo <hoanhtuan.vo@gmail.com>
2025-06-19 02:03:48 +0700

    added Comparator

--- src/com/collections/StudentCollectionRunner.java
+++ src/com/collections/StudentCollectionRunner.java
@@ -2,7 +2,18 @@ package com.collections;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+
+class DescendingStudentComparator implements Comparator<Student> {
+
+	@Override
+	public int compare(Student o1, Student o2) {
+		// TODO Auto-generated method stub
+		return Integer.compare(o2.getId(), o1.getId());
+	}
+	
+}
 
 public class StudentCollectionRunner {
 
@@ -15,7 +26,13 @@ public class StudentCollectionRunner {
 		List<Student> studentAl = new ArrayList<>(students);
 		
 		System.out.println(students);
+		System.out.println();
+
 		Collections.sort(studentAl);
+		System.out.println(studentAl);
+		
+		System.out.println();
+		Collections.sort(studentAl, new DescendingStudentComparator());
 		System.out.println(studentAl);
 	}
 
